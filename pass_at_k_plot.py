2c45d80de4dd3bca4f9b6b1afdc2f41ce540910d
Benedikt Stroebl <stroebl@princeton.edu>
2025-02-11 04:22:02 -0500

    plotting code

--- pass_at_k_plot.py
+++ pass_at_k_plot.py
@@ -37,24 +37,26 @@ def calculate_pass_rates(directory):
 
 def main():
     directories = [
-        "/scratch/gpfs/bs6865/messi-thinking/SkyThought/samples/taco-ll3-8B-llama-8b-big-lora-256-epochs-2-merged",
-        "/scratch/gpfs/bs6865/messi-thinking/SkyThought/samples/taco-ll3-8B-llama-8b-slerp-0.7-big-lora-256-epochs-2-merged",
-        "/scratch/gpfs/bs6865/messi-thinking/SkyThought/samples/taco-ll3-8B-llama-8b-slerp-0.5-big-lora-256-epochs-2-merged"
+        "SkyThought/samples/taco-ll3-8B-llama-8b-big-lora-256-epochs-2-merged",
+        "SkyThought/samples/taco-ll3-8B-llama-8b-slerp-0.7-big-lora-256-epochs-2-merged",
+        "SkyThought/samples/taco-ll3-8B-llama-8b-big-slerp-0.5-lora-256-epochs-2-merged"
     ]
 
     k_values = list(range(1, 11))
     plt.figure(figsize=(8, 6))
     
     names = {
-        "/scratch/gpfs/bs6865/messi-thinking/SkyThought/samples/taco-ll3-8B-llama-8b-big-lora-256-epochs-2-merged": r"$\alpha=1$",
-        "/scratch/gpfs/bs6865/messi-thinking/SkyThought/samples/taco-ll3-8B-llama-8b-slerp-0.7-big-lora-256-epochs-2-merged": r"$\alpha=0.7$",
-        "/scratch/gpfs/bs6865/messi-thinking/SkyThought/samples/taco-ll3-8B-llama-8b-slerp-0.5-big-lora-256-epochs-2-merged": r"$\alpha=0.5$"
+        "SkyThought/samples/taco-ll3-8B-llama-8b-big-lora-256-epochs-2-merged": r"$\alpha=1$",
+        "SkyThought/samples/taco-ll3-8B-llama-8b-slerp-0.7-big-lora-256-epochs-2-merged": r"$\alpha=0.7$",
+        "SkyThought/samples/taco-ll3-8B-llama-8b-big-slerp-0.5-lora-256-epochs-2-merged": r"$\alpha=0.5$"
     }
     
     for directory in directories:
         pass_rates = calculate_pass_rates(directory)
         label = names.get(directory, os.path.basename(os.path.normpath(directory)))
-        sns.lineplot(x=k_values, y=pass_rates, marker="o", label=label, linewidth=3, markersize=8)
+        # print it in a nice format
+        print(f"{label}: {pass_rates}")
+        sns.lineplot(x=k_values, y=pass_rates, marker="o", label=label, linewidth=3, markersize=10)
     
     plt.xlabel("k", fontsize=22)
     plt.ylabel("Pass@k", fontsize=22)
@@ -62,8 +64,9 @@ def main():
     plt.yticks(np.arange(0, 0.41, 0.1), fontsize=22)
     plt.ylim(0, 0.4)
     plt.grid(False)
-    plt.legend(fontsize=20)
+    plt.legend(fontsize=20, frameon=False, loc="lower right")
     plt.tight_layout()
+    sns.despine()
     plt.savefig("finetuning_pass_at_k.pdf")
     plt.show()
 
